c0328f02fefcf518ea555687119e947bf1ef2247
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-11 19:03:34 +0100

    setup: Inform about progress

--- setup.sh
+++ setup.sh
@@ -3,16 +3,17 @@ MYSQL_ROOT_PASSWORD="secret one"
 docker pull sils1297/centos-owncloud
 docker pull mysql
 
-# Mysql container
+echo "starting mysql container..."
 docker run --name="owncloud-mysql" --restart=always \
 -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -d mysql
 
-# Owncloud container, linked to mysql container
+echo "starting owncloud container..."
 docker run --name="owncloud" --restart=always --link owncloud-mysql:mysql -d \
 -p 40080:80 -p 48080:8080 sils1297/centos-owncloud
 
-# Data containers
+echo "starting owncloud mysql data container..."
 docker run --name="owncloud-mysql-data" --volumes-from owncloud-mysql \
 sils1297/centos /bin/echo mysql owncloud data container
+echo "starting owncloud data container..."
 docker run --name="owncloud-data" --volumes-from owncloud \
 sils1297/centos /bin/echo owncloud data container
