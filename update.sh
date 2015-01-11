62dff0c14e69237ad174f2fde11636b485ad5974
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-11 19:23:25 +0100

    Add update script

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Updating images..."
+docker pull sils1297/centos-owncloud
+docker pull mysql
+
+echo "Restarting mysql container..."
+docker rm -f owncloud-mysql
+docker run --name="owncloud-mysql" --restart=always --volumes-from \
+owncloud-mysql-data -d mysql
+
+echo "Restarting owncloud container..."
+docker rm -f owncloud
+docker run --name="owncloud" --restart=always --link owncloud-mysql:mysql -d \
+--volumes-from owncloud-data -p 40080:80 -p 48080:8080 sils1297/centos-owncloud
