893dfaff347212fd7ebeeda86310e9ad597aafa6
cw1 <cw1@users.noreply.github.com>
2015-01-12 13:59:22 +0100

    Update update.sh
    using variables from owncloud-config.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+source owncloud-config.sh
+
 echo "Updating images..."
 docker pull sils1297/centos-owncloud
 docker pull sils1297/mysql
 
 echo "Restarting mysql container..."
-docker rm -f owncloud-mysql
-docker run --name="owncloud-mysql" --restart=always --volumes-from \
-owncloud-mysql-data -d sils1297/mysql
+docker rm -f $MYSQL_NAME
+docker run --name="$MYSQL_NAME" --restart=always \
+--volumes-from $MYSQL_DATA_NAME -d sils1297/mysql
 
 echo "Restarting owncloud container..."
-docker rm -f owncloud
-docker run --name="owncloud" --restart=always --link owncloud-mysql:mysql -d \
---volumes-from owncloud-data -p 40080:80 -p 48080:8080 sils1297/centos-owncloud
+docker rm -f $OWNCLOUD_NAME
+docker run --name="$OWNCLOUD_NAME" --restart=always --link $MYSQL_NAME:mysql -d \
+--volumes-from $OWNCLOUD_DATA_NAME -p 40080:80 -p 48080:8080 sils1297/centos-owncloud
